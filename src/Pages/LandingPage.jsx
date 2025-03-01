07e33bfbba81133a78497fd7e95d645a30c93fe0
adilraees786 <adilraees911@gmail.com>
2025-03-01 15:34:44 +0500

    change margan

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -30,7 +30,7 @@ const LandingPage = () => {
         {/* Loan Categories Section */}
         <section className="py-16 bg-gray-100">
           <div className="container mx-auto px-6">
-            <h3 className="text-3xl font-bold text-center text-gray-800 mb-10">
+            <h3 className="text-3xl font-bold text-center text-gray-800 mb-8">
               Our Loan Categories
             </h3>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
